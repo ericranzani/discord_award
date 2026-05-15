bf7f4b8bfe8368d2d5ae4e4445e92e8266a60d19
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2026-05-15 16:14:01 -0300

    criado todo o corpo do main.py e validações dos enpoit pelo swagger

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from typing import List
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from . import models, schemas
+from .database import engine, get_db
 
-app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Discord Awards API")
 
 origins = [
     "http://localhost:4200",
@@ -17,6 +23,36 @@ app.add_middleware(
     allow_headers=["*"],              # Permite todos os cabeçalhos
 )
 
+# --- ROTAS (ENDPOINTS) ---
+
 @app.get("/")
 def read_root():
-    return {"status": "Docker operando!", "projeto": "Discord Awards"}
+    return {"message": "API do Discord Awards rodando com sucesso!"}
+
+# Rota para Criar uma Categoria
+@app.post("/categorias/", response_model=schemas.Categoria)
+def criar_categoria(categoria: schemas.CategoriaCreate, db: Session = Depends(get_db)):
+    db_categoria = models.Categoria(nome=categoria.nome, descricao=categoria.descricao)
+    db.add(db_categoria)
+    db.commit()
+    db.refresh(db_categoria)
+    return db_categoria
+
+# Rota para Listar todas as Categorias
+@app.get("/categorias/", response_model=List[schemas.Categoria])
+def listar_categorias(db: Session = Depends(get_db)):
+    return db.query(models.Categoria).all()
+
+# Rota para Adicionar Candidato a uma Categoria
+@app.post("/candidatos/", response_model=schemas.Candidato)
+def criar_candidato(candidato: schemas.CandidatoCreate, categoria_id: int, db: Session = Depends(get_db)):
+    # Verifica se a categoria existe antes de adicionar o candidato
+    db_categoria = db.query(models.Categoria).filter(models.Categoria.id == categoria_id).first()
+    if not db_categoria:
+        raise HTTPException(status_code=404, detail="Categoria não encontrada")
+    
+    db_candidato = models.Candidato(nome=candidato.nome, categoria_id=categoria_id)
+    db.add(db_candidato)
+    db.commit()
+    db.refresh(db_candidato)
+    return db_candidato
